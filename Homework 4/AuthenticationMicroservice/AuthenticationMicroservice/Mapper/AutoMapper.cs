b5fdf051dec839040f1d0399929662edf1cb8645
Jan Stankov <jan.stankov@gmail.com>
2024-01-28 16:24:27 +0100

    Create AutoMapper.cs

---
+++ Homework 4/AuthenticationMicroservice/AuthenticationMicroservice/Mapper/AutoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AuthenticationMicroservice.Models;
+using AuthenticationMicroservice.Models.Responses;
+
+namespace AuthenticationMicroservice.Mapper
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<User, RegisterResponse>();
+        }
+    }
+}
