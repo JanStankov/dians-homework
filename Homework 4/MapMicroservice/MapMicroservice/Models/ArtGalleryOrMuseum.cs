977b21c47875be9aacf35daf0c15dc2f78ea651f
Jan Stankov <jan.stankov@gmail.com>
2024-01-28 16:49:13 +0100

    Create ArtGalleryOrMuseum.cs

---
+++ Homework 4/MapMicroservice/MapMicroservice/Models/ArtGalleryOrMuseum.cs
@@ -0,0 +1,17 @@
+﻿namespace MapMicroservice.Models
+{
+    public class ArtGalleryOrMuseum
+    {
+        public long Id { get; set; }
+        public string Type { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public Dictionary<string, string> Tags { get; set; }
+        public string Name { get; set; }
+        public string NameEn { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public string Address { get; set; }
+        public string Wikipedia { get; set; }
+    }
+}
