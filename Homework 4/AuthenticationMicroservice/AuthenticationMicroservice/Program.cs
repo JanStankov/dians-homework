1a3559b9cd9a5e96a63ce202606c05d242c46a37
Jan Stankov <jan.stankov@gmail.com>
2024-01-28 16:27:29 +0100

    Update Program.cs

--- Homework 4/AuthenticationMicroservice/AuthenticationMicroservice/Program.cs
+++ Homework 4/AuthenticationMicroservice/AuthenticationMicroservice/Program.cs
@@ -1,11 +1,71 @@
+using Microsoft.EntityFrameworkCore;
+using AuthenticationMicroservice.Database;
+using AuthenticationMicroservice.Database.Repositories;
+using AuthenticationMicroservice.Database.Repositories.Interfaces;
+using AuthenticationMicroservice.Services;
+using AuthenticationMicroservice.Utilities;
+using AuthenticationMicroservice.Utilities.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AuthenticationMicroservice.Services.Interfaces;
+using AuthenticationMicroservice.Models;
+using AuthenticationMicroservice.Database.Repositories.Interfaces;
+using AuthenticationMicroservice.Database.Repositories;
+using AuthenticationMicroservice.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var settings = new Settings();
+builder.Configuration.Bind("Settings", settings);
+builder.Services.AddSingleton(settings);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(settings.BearerKey)),
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = false,
+        ValidateIssuer = false
+    };
+});
+
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddHttpClient();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+var connectionString = builder.Configuration.GetConnectionString("Db");
+builder.Services.AddDbContext<AuthenticationMicroserviceDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                              .AllowAnyMethod()
+                              .AllowAnyHeader();
+                      });
+});
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
 
 var app = builder.Build();
 
@@ -18,6 +78,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
