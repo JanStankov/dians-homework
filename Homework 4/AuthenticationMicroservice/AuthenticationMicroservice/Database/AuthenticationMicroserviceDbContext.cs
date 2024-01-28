e618937ebc0a241efa6c8061c516513f4177d359
Jan Stankov <jan.stankov@gmail.com>
2024-01-28 16:22:42 +0100

    Add user repository

---
+++ Homework 4/AuthenticationMicroservice/AuthenticationMicroservice/Database/AuthenticationMicroserviceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuthenticationMicroservice.Models;
+
+namespace AuthenticationMicroservice.Database
+{
+    public class AuthenticationMicroserviceDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AuthenticationMicroserviceDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Homework 4/AuthenticationMicroservice/AuthenticationMicroservice/Database/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using AuthenticationMicroservice.Models;
+using AuthenticationMicroservice.Models.Commands;
+
+namespace AuthenticationMicroservice.Database.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User> CreateUser(CreateUserCommand createUserCommand);
+
+        public Task<User> GetUserByEmail(string email);
+    }
+}
